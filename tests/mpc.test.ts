3921569aec712e9bcf8a2a0382983451acf3b180
Andrew Morris <voltrevo@gmail.com>
2025-06-13 14:00:38 +1000

    add tests

--- tests/mpc.test.ts
+++ tests/mpc.test.ts
@@ -173,6 +173,133 @@ test('boolean io', async () => {
   ]);
 });
 
+const getSha256Circuit = (() => {
+  let compileOutput: summon.CompileResult | undefined = undefined;
+
+  return async () => {
+    if (compileOutput) {
+      return compileOutput;
+    }
+
+    await summon.init();
+
+    const libVersion = 'c24b5f32ccb8d8ffe77fb1465425a0575012b4b7';
+    const ghPse = 'https://raw.githubusercontent.com/privacy-scaling-explorations';
+    const libBase = `${ghPse}/summon-lib/${libVersion}`;
+
+    async function downloadLib(path: string) {
+      const resp = await fetch(`${libBase}/${path}`);
+      const txt = await resp.text();
+
+      return txt;
+    }
+
+    compileOutput = summon.compile({
+      path: '/src/main.ts',
+      boolifyWidth: 8,
+      files: {
+        '/src/main.ts': `
+          import sha256 from './deps/sha256/mod.ts';
+
+          export default (io: Summon.IO) => {
+            // 6 characters * 8 bits, so we can encode 'summon'
+            const input = range(6 * 8).map(
+              i => io.input('alice', \`input\${i}\`, summon.bool()),
+            );
+
+            io.addParty('bob');
+
+            // expected: 2815cb02b95b6d15383bf551f09b33e01806ad2f4221b035a592c1be146d6a99
+            // (but encoded as bits)
+            const output = sha256(input);
+
+            for (const [i, b] of output.entries()) {
+              io.outputPublic(\`output\${i}\`, b);
+            }
+          };
+
+          export function range(limit: number) {
+            let res = [];
+
+            for (let i = 0; i < limit; i++) {
+              res.push(i);
+            }
+
+            return res;
+          }
+        `,
+        '/src/deps/sha256/mod.ts': await downloadLib('sha256/mod.ts'),
+        '/src/deps/sha256/sha256Compress.ts': await downloadLib('sha256/sha256Compress.ts'),
+      },
+    });
+
+    return compileOutput;
+  };
+})();
+
+test('compile sha256', async () => {
+  await getSha256Circuit();
+});
+
+test("sha256('summon') == 28..99", { skip: true }, async () => {
+  const { circuit } = await getSha256Circuit();
+
+  const protocol = new Protocol(circuit, new EmpWasmEngine());
+  const aqs = new AsyncQueueStore<Uint8Array>();
+
+  const summonBits = [...'summon']
+    .map(c => c.codePointAt(0)!.toString(2).padStart(8, '0'))
+    .join('').split('')
+    .map(bit => bit === '0' ? false : true);
+
+  const aliceInputs = Object.fromEntries(summonBits.entries().map(
+    ([i, boolBit]) => [`input${i}`, boolBit],
+  ));
+
+  const outputs = await Promise.all([
+    runParty(protocol, 'alice', aliceInputs, aqs),
+    runParty(protocol, 'bob', {}, aqs),
+  ]);
+
+  for (const output of outputs) {
+    const bits = range(256).map(i => output[`output${i}`] as boolean);
+    const outputHex = bitsToHex(bits);
+
+    expect(outputHex).to.eq('2815cb02b95b6d15383bf551f09b33e01806ad2f4221b035a592c1be146d6a99');
+  }
+});
+
+function range(limit: number) {
+  let res: number[] = [];
+
+  for (let i = 0; i < limit; i++) {
+    res.push(i);
+  }
+
+  return res;
+}
+
+function bitsToHex(bits: boolean[]) {
+  if (bits.length % 8 !== 0) {
+    throw new Error('bits do not form complete bytes');
+  }
+
+  let res = '';
+
+  for (let i = 0; i < bits.length; i += 8) {
+    let byte = 0;
+
+    for (let j = 0; j < 8; j++) {
+      const bit = Number(bits[i + j]);
+      byte |= bit << (7 - j);
+    }
+
+    res += byte.toString(16).padStart(2, '0');
+  }
+
+  return res;
+}
+
 async function runParty(
   protocol: Protocol,
   party: string,
